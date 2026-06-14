e346bac4dacfce86375988868e4cb941a38b048d
SamCherri <36168102+SamCherri@users.noreply.github.com>
2026-06-14 12:13:18 -0300

    fix: type item action table autoload implementation

--- item/item_action_table_autoload.gd
+++ item/item_action_table_autoload.gd
@@ -13,47 +13,47 @@ var _implementation: Node
 
 
 func setup(inventory, show_spin_box: Callable) -> void:
-	var implementation := _get_implementation()
+	var implementation: Node = _get_implementation()
 	if implementation == null:
 		return
 	implementation.setup(inventory, show_spin_box)
 
 
 func create_options(item) -> Array:
-	var implementation := _get_implementation()
+	var implementation: Node = _get_implementation()
 	if implementation == null:
 		return []
 	return implementation.create_options(item)
 
 
 func create_reload_options(ammo) -> Array:
-	var implementation := _get_implementation()
+	var implementation: Node = _get_implementation()
 	if implementation == null:
 		return []
 	return implementation.create_reload_options(ammo)
 
 
 func get_recipes(item1, item2) -> Array:
-	var implementation := _get_implementation()
+	var implementation: Node = _get_implementation()
 	if implementation == null:
 		return []
 	return implementation.get_recipes(item1, item2)
 
 
 func create_crafting_options(instance1_id: int, instance2_id: int) -> Array:
-	var implementation := _get_implementation()
+	var implementation: Node = _get_implementation()
 	if implementation == null:
 		return []
 	return implementation.create_crafting_options(instance1_id, instance2_id)
 
 
-func _get_implementation():
+func _get_implementation() -> Node:
 	if DEDICATED_SERVER_ARGUMENT in OS.get_cmdline_user_args():
 		return null
 	if is_instance_valid(_implementation):
 		return _implementation
 
-	var implementation_script = load(IMPLEMENTATION_PATH)
+	var implementation_script: Script = load(IMPLEMENTATION_PATH) as Script
 	if implementation_script == null:
 		push_error("ItemActionTable: não foi possível carregar a implementação.")
 		return null
